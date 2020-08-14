91f75d4b7ee3ce5f8d51636f0ff682bd0e637dad
Keegan Green <39037966+keeganmjgreen@users.noreply.github.com>
2020-08-13 23:11:25 -0700

    Create incline_theta_U.m

---
+++ incline_theta_U.m
@@ -0,0 +1,52 @@
+% Uphill ('incline') - Upper Leg ('U')
+
+function pp = incline_theta_U()
+
+addpath code
+
+load('data\incline_theta_U.mat')
+
+Y = smooth(Orientation.Y - 82.8980 + 90);
+
+[pks, locs] = findpeaks(+Y);
+
+pks_locs = table2array(sortrows(array2table(topkrows([pks, locs], 38, 1)), 2));
+
+pks  = +pks_locs(:, 1);
+locs = +pks_locs(:, 2);
+
+my_new_figure()
+
+xData = [];
+yData = [];
+
+for i = 1 : length(locs) - 1
+
+    x{i} = (0 : (locs(i + 1) - locs(i)))' / (locs(i + 1) - locs(i));
+
+    y{i} = Y(locs(i) : locs(i + 1));
+
+    plot(x{i}, y{i}, '-', 'Color', '#CFD8DC', 'HandleVisibility', 'off')
+
+    xData = [xData; x{i}];
+    yData = [yData; y{i}];
+
+end
+
+pp = csaps([xData; xData + 1], [yData; yData], 1 - 1e-5);
+
+fplot(@(xq) ppval(pp, xq + 1), [0, 0.5], 'k', 'LineWidth', 1.5)
+fplot(@(xq) ppval(pp, xq + 0), [0.5, 1], 'k', 'LineWidth', 1.5)
+
+my_legend(' Cubic Smoothing Spline ')
+
+my_ticks([0, 90, 180])
+
+my_xlabel('Arbitrary Fraction of Gait Cycle')
+my_ylabel('Gyroscope Pitch = $ \theta $ Below $ xy \, $-Plane (deg.)')
+
+my_title('\textbf{Uphill --- Upper Leg}')
+
+print plots\incline_theta_U -dsvg
+
+clear i my_axes
