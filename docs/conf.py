c087f7c7574a7c432c43f8637bf7f1c806eacb46
Keegan Green <39037966+keeganmjgreen@users.noreply.github.com>
2021-09-06 21:53:37 -0400

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Keegan Green'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxemoji.sphinxemoji'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
